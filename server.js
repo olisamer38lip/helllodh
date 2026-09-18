bf9724a1ea287789fb755db5cebc34ff5869ab4d
moufasarim-lang <moufasarim@gmail.com>
2026-09-18 13:16:46 -0400

    Fix deployment: adjust server.listen and package.json build script

--- server.js
+++ server.js
@@ -97,6 +97,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(PORT, '0.0.0.0', () => {
+server.listen(PORT, () => {
   console.log(`DHL app running on port ${PORT}`);
 });
